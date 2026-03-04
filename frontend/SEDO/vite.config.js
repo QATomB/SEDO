d7646717c14774672e0ae0108da7d5480990b5b2
QATomB <147164443+QATomB@users.noreply.github.com>
2026-03-04 15:58:12 +0000

    Update vite.config.js to remove server proxy

--- frontend/SEDO/vite.config.js
+++ frontend/SEDO/vite.config.js
@@ -4,24 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://sedo-backend.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\//, ""),
-        configure: (proxy) => {
-          proxy.on("error", (err) => {
-            console.log("proxy error: ", err);
-          });
-          proxy.on("proxyRes", (proxyRes, req) => {
-            console.log("response from target:", proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
-  },
   test: {
     globals: true,
     setupFiles: "./src/tests/setup.ts",
